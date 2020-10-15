1c561e6f8dd48c0135140300212e691dd98fd5b6
Eduardo Cotta <eduardo.cotta@dtidigital.com.br>
2020-10-15 11:52:33 -0300

    Change project structure

---
+++ src/Models/Service/IGithubRepositoryService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TakeGithubAPI.DTO;
+
+namespace TakeGithubAPI.Models.Service
+{
+    public interface IGithubRepositoryService
+    {
+        List<GithubRepositoryDTO> GetAllGithubRepositories();
+        List<GithubRepositoryDTO> GetFirstFiveGithubRepositoriesLanguageCSharp();
+    }
+}
