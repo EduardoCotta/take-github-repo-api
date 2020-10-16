3e56573fda3769a061a90cf31e316b1ff2ddcdd6
Eduardo Cotta <eduardo.cotta@dtidigital.com.br>
2020-10-16 01:41:42 -0300

    feat: add swagger to project

--- src/Startup.cs
+++ src/Startup.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using System;
 using TakeGithubAPI.Models.Repository;
 using TakeGithubAPI.Models.Service;
 using TakeGithubAPI.Repository;
@@ -27,6 +29,22 @@ namespace TakeGithubAPI
             services.AddScoped<IGithubRepoService, GithubRepoService>();
             services.AddScoped<IGithubRepoRepository, GithubRepoRepository>();
             services.AddScoped<IApplicationEnviroment, ApplicationEnviroment>();
+
+            services.AddSwaggerGen(c => {
+
+                c.SwaggerDoc("v1",
+                    new OpenApiInfo
+                    {
+                        Title = "Github Repository Manager API",
+                        Version = "v1",
+                        Description = "Essa é uma API de demonstração construída com .NET Core utilizando C#. Seu objetivo principal é retornar os repositórios mais antigos da Take.net que utilizam C#.",
+                        Contact = new OpenApiContact
+                        {
+                            Name = "Eduardo Cotta",
+                            Url = new Uri("https://github.com/EduardoCotta")
+                        }
+                    });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,6 +54,11 @@ namespace TakeGithubAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Github Repository Manager API V1");
+            });
 
             app.UseHttpsRedirection();
 
